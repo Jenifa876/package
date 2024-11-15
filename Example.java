6030fc29bd1c4d44bdb5e87f2c2181d8cadb6b33
Jenifa876 <jenivedha2004@gmail.com>
2024-11-15 21:46:47 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+import MyPackage.MyClass;
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass o=new MyClass();
+		o.set_value(10);
+		o.display_value();
+	}
+
+}
---
+++ MyClass.java
@@ -0,0 +1,13 @@
+package MyPackage;
+
+public class MyClass {
+	int a;
+	public void set_value(int n)
+	{
+		a=n;
+	}
+	public void display_value()
+	{
+		System.out.println("The value of a: "+a);
+	}
+}
